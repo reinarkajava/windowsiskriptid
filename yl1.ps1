60bc68fca854b3e7df1a406611a49df585509e98
reinarkajava <118804317+reinarkajava@users.noreply.github.com>
2022-11-24 15:29:39 +0200

    Windowsi Skript 1
    Kasutaja lisamine

---
+++ yl1.ps1
@@ -0,0 +1,28 @@
+﻿
+
+#Skript kysib kasutaja ees ja perekonnanime
+    $KasutajaNimi = Read-Host 'Sisesta Eesnimi'
+    $Perekonnanimi = Read-Host 'Sisesta Perekonnanimi'
+#Kasutades ToLower funktsiooni muudetakse suured tahed vaikesteks
+    $KasEesnimi = $KasutajaNimi.ToLower()
+    $KasPerenimi = $Perekonnanimi.ToLower()
+    
+#Konto Kirlejdus ja parooli parameeter
+
+    $KontoKirjeldus = "Skripti Jaoks"
+    $Parool = ConvertTo-SecureString "Parool1!" -AsPlainText -Force
+
+
+#Saadud info alusel tehakse uued kasutajad
+    
+    
+    echo "Luuakse kasutaja $KasutajaNimi $Perekonnanimi."
+    New-LocalUser -Name $KasEesnimi"."$KasPerenimi -Password $Parool -Description "$KontoKirjeldus"
+    
+Powershelli veateate asendamine
+    $ErrorActionPreference = 'SilentlyContinue'
+  try { NonsenseString }
+catch {
+  Write-Host "Viga kasutaja loomisel"
+
+}
