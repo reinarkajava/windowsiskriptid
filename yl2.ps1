475c399e824fa3c1c6f82dd5f3b399c771e310af
reinarkajava <118804317+reinarkajava@users.noreply.github.com>
2022-11-25 13:56:30 +0200

    Add files via upload

---
+++ yl2.ps1
@@ -0,0 +1,29 @@
+﻿#Skript kysib kasutaja ees ja perekonnanime
+    $KasutajaNimi = Read-Host 'Sisesta Eesnimi'
+    $Perekonnanimi = Read-Host 'Sisesta Perekonnanimi'
+#Kasutades ToLower funktsiooni muudetakse suured tahed vaikesteks
+    $KasEesnimi = $KasutajaNimi.ToLower()
+    $KasPerenimi = $Perekonnanimi.ToLower()
+    
+#Konto Kirlejdus ja parooli parameeter
+
+    $KontoKirjeldus = "Skripti Jaoks"
+    $Parool = ConvertTo-SecureString "Parool1!" -AsPlainText -Force
+
+
+#Saadud info alusel tehakse uued kasutajad
+    
+    
+    echo "Eemaldatakse kasutaja $KasutajaNimi.$Perekonnanimi."
+    Remove-LocalUser -Name $KasEesnimi"."$KasPerenimi
+     try { Remove-LocalUser }
+    catch {
+       Write-Host "Kasutaja on eemaldatud"
+      }
+ 
+    Powershelli veateate asendamine
+    $ErrorActionPreference = 'SilentlyContinue'
+    try { NonsenseString }
+    catch {
+    Write-Host "Viga kasutaja eemaldamisel"
+    }
