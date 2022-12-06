b9f4317eb35bf6f4760664a43a90e9053d28bee1
reinarkajava <118804317+reinarkajava@users.noreply.github.com>
2022-12-06 10:05:47 +0200

    Windowsi Skriptimine ülesanne 4
    Skript loob domeeni kasutajad kasutades kasutajad .csv faili ning kontrollib kas kasutaja on eelnevalt loodud.

---
+++ yl4.ps1
@@ -0,0 +1,39 @@
+﻿#Enter a path to your import CSV file
+#$ADUsers = Import-csv C:\Users\Administrator\Documents\kasutajad.csv
+$users = Import-Csv C:\Users\Administrator\Documents\kasutajad.csv -Encoding Default -Delimiter ";"
+foreach ($User in $users)
+{
+
+       #$Username    = $user.username
+       $Password    = $user.password
+       $Firstname   = $user.firstname
+       $Lastname    = $user.lastname
+       $username = $user.FirstName + "." + $user.LastName
+       $username = $username.ToLower()
+       $Department =  $user.department
+      # $OU           = $User.ou
+
+       #Check if the user account already exists in AD
+       if (Get-ADUser -F {SamAccountName -eq $username})
+       {
+               #If user does exist, output a warning message
+               Write-Warning "A user account $Username has already exist in Active Directory."
+       }
+       else
+       {
+              #If a user does not exist then create a new user account
+        
+             New-ADUser `
+            -SamAccountName $Username `
+            -UserPrincipalName "$Username@sv-koolr" `
+            -Name "$Firstname $Lastname" `
+            -GivenName $Firstname `
+            -Surname $Lastname `
+            -Enabled $True `
+            -ChangePasswordAtLogon $True `
+            -DisplayName "$Lastname, $Firstname" `
+            -Department $Department `
+            -AccountPassword (convertto-securestring $Password -AsPlainText -Force)
+
+       }
+}
